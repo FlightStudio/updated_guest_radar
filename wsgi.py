a283a55ae437118dff39d14d8fad72ecf790d18c
MTFlightStudio <matt@flightstudio.com>
2024-09-30 12:20:44 +0100

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,9 @@
+import sys
 from app import app
 
 if __name__ == "__main__":
-    app.run()
+    try:
+        app.run()
+    except Exception as e:
+        print(f"An error occurred: {e}", file=sys.stderr)
+        raise
